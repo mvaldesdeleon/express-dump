44925f296144744ffaf49de94b06f78df2608f7e
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-24 22:02:34 +0200

    Ensures path is always created.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const pify = require('pify');
+const mkdirp = require('mkdirp');
 const { writeFile } = require('fs');
 const { json } = require('body-parser');
 
+const removeLast = fullPath => fullPath.split('/').slice(0, -1).join('/') || '/';
+
 module.exports = function(options = {}) {
-    const basePath = options.basePath ? `/${options.basePath}`: '';
+    const basePath = options.path ? `${__dirname}/${options.path}`: __dirname;
     const parser = json({
         limit: typeof options.limit === 'undefined' ? '10kb' : options.limit,
         type: () => true
     });
     const logs = {};
 
+    const getFullPath = path => `${__dirname}${basePath}${path}`;
+    const ensurePath = path => pify(mkdirp)(removeLast(getFullPath(path)));
+    const writePathToFile = path => pify(writeFile)(`${getFullPath(path)}.json`, JSON.stringify(logs[path]));
+
     const log = (key, entry) => key in logs ? logs[key].push(entry) : logs[key] = [entry];
-    const dumpPath = path => pify(writeFile)(`${__dirname}${basePath}${path}.json`, JSON.stringify(logs[path]));
+    const dumpPath = path => ensurePath(path).then(() => writePathToFile(path));
 
     const middleware = (req, res) => {
         log(req.path, req.body);
